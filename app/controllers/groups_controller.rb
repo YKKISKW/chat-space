293cd6333a9404b46ec9b8dae714672d77d19f17
YKKISKW <yukako1160101@gmail.copm>
2019-02-16 19:48:25 +0900

    Edit group new
    グループ新規作成画面の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,22 @@
 class GroupsController < ApplicationController
+
+  def index
+  end
+
   def new
+    @group = Group.new
+    @group.users << current_user
+
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      binding.pry
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
 
   end
 
@@ -12,5 +26,10 @@ class GroupsController < ApplicationController
 
   def update
 
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
