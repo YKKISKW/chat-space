2a6195f160fdadf5d4536efa99cc0c49e2aac36b
YKKISKW <yukako1160101@gmail.copm>
2019-02-16 20:46:07 +0900

    Edit group edit&upddate
    グループ編集画面の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit,:update]
 
   def index
   end
@@ -6,7 +7,6 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @group.users << current_user
-
   end
 
   def create
@@ -16,19 +16,24 @@ class GroupsController < ApplicationController
     else
       render :new
     end
-
   end
 
-  def edit
-
-  end
 
   def update
-
+    if group.update(group_params)
+      redirect_to root_path, notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
+
+  def set_group
+        @group = Group.find(params[:id])
+  end
+
 end
